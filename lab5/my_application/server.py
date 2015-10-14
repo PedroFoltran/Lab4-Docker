ded172aa1433f94ce2c9409d0956146760e09e2d
Pedro Foltran <pedrofoltran@hotmail.com>
2015-10-14 18:15:20 +0300

    Chaging Server.py

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -2,8 +2,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+   	 return "Index File"
+
+@app.route("/hello")
 def hello():
-    return "Hello World!"
+    	return "Hello Word"
+
+@app.route("/user/<username>")
+def user(username):
+    	return "User %s" % username
+
+@app.route("/post/<int:post_id>")
+def post(post_id):	
+	return "Post %d" % post_id
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",debug=True)
